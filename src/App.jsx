78b017a21057f41d5ebe8f1deda677204d61c4a4
daffalde <daffalderayhan@gmail.com>
2025-05-12 08:28:07 +0700

    pembuatan file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home.jsx";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
